dcba9ded24c721216e2e1196ae0da0b326293b6d
fuyutarow <fuyutarow@gmail.com>
2023-09-20 21:14:09 +0900

    [ui] Add MoveCallCard

--- ui/src/app/admin/page.tsx
+++ ui/src/app/admin/page.tsx
@@ -2,78 +2,13 @@
 
 import { useWallet } from '@suiet/wallet-kit';
 import { AppBar } from 'src/components/AppBar';
-import { TransactionBlock } from '@mysten/sui.js/transactions'
 import { STSUI_COIN } from 'src/moveCall/frostend/stsui-coin/structs';
 import { createBank, createVault } from 'src/moveCall/frostend/root/functions';
 import { BANK, ROOT, VAULT } from 'src/config/frostend';
 import Link from 'next/link';
 import { moveCallFaucet } from 'src/frostendLib';
-import { toast } from 'react-hot-toast';
-import { noticeTxnResultMessage } from 'src/components/TransactionToast';
-import { MouseEventHandler } from 'react';
 import { sharbetMoveCall } from 'src/sharbetLib';
-
-const Card = (props: {
-  title: string,
-  buttonText: string,
-  onClick: MouseEventHandler<HTMLButtonElement> | undefined
-}) => (
-  <div className='bg-gray-100 px-3 py-2 rounded-lg w-[200px] h-[200px] flex items-center justify-center'>
-    <div className='flex flex-col items-center gap-3'>
-      <div className='text-black text-lg font-bold'>
-        {props.title}
-      </div>
-      <button
-        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full"
-        onClick={props.onClick}
-      >
-        {props.buttonText}
-      </button>
-    </div>
-  </div>
-);
-
-const FaucetCard = (props: {
-  amount: bigint,
-  coinType: string,
-  display: string,
-  buttonDisplay: string,
-}) => {
-  const wallet = useWallet();
-
-  const executeTransaction = async () => {
-    const txb = new TransactionBlock();
-    moveCallFaucet(txb, { amount: props.amount })
-
-    const r = await wallet.signAndExecuteTransactionBlock({
-      // @ts-ignore
-      transactionBlock: txb
-    });
-    noticeTxnResultMessage(r);
-  };
-
-  return <Card title={props.display} buttonText={props.buttonDisplay} onClick={executeTransaction} />;
-};
-
-const VaultAndBankCard = () => {
-  const wallet = useWallet();
-
-  const executeTransaction = async () => {
-    const txb = new TransactionBlock();
-
-    createBank(txb, STSUI_COIN.$typeName, ROOT);
-    createVault(txb, STSUI_COIN.$typeName, ROOT);
-
-    const r = await wallet.signAndExecuteTransactionBlock({
-      // @ts-ignore
-      transactionBlock: txb
-    });
-    noticeTxnResultMessage(r);
-  };
-
-  return <Card title="new Bank and Vault for stSUI" buttonText="create" onClick={executeTransaction} />;
-};
-
+import { MoveCallCard } from 'src/components/MoveCallCard';
 
 const ViewObject = (props: {
   objectId: string,
@@ -99,41 +34,48 @@ const ViewObject = (props: {
   );
 };
 
-const StakeCard = () => {
+const Page = () => {
   const wallet = useWallet();
 
-  const executeTransaction = async () => {
-    if (!wallet.address) return;
-    const txb = new TransactionBlock();
-
-    await sharbetMoveCall.stakeSuiToMintShasui(txb, {
-      address: wallet.address!,
-      amount: BigInt(100),
-    })
-
-    const r = await wallet.signAndExecuteTransactionBlock({
-      // @ts-ignore
-      transactionBlock: txb
-    });
-    noticeTxnResultMessage(r);
-  };
-
-  return <Card title="Satke SUI" buttonText="deposit" onClick={executeTransaction} />;
-};
-
-const Page = () => {
   return (
     <div className="h-screen bg-blue-500">
       <AppBar />
       <main className="flex mx-auto items-center mt-[120px] max-w-[90%]">
         <div className="text-white flex flex-wrap gap-10">
-          <FaucetCard amount={BigInt(100e8)} coinType={STSUI_COIN.$typeName} display='stSUI 100' buttonDisplay='faucet' />
-          <FaucetCard amount={BigInt(100e3 * 1e8)} coinType={STSUI_COIN.$typeName} display='stSUI 100_000' buttonDisplay='faucet' />
-          <VaultAndBankCard />
+          <MoveCallCard
+            title='stSUI 100'
+            buttonText='faucet'
+            moveCall={async (txb) => {
+              await moveCallFaucet(txb, { amount: BigInt(100e8) })
+            }}
+          />
+          <MoveCallCard
+            title='stSUI 100'
+            buttonText='faucet'
+            moveCall={async (txb) => {
+              await moveCallFaucet(txb, { amount: BigInt(100e3 * 1e8) })
+            }}
+          />
+          <MoveCallCard
+            title="new Bank and Vault for stSUI"
+            buttonText="create"
+            moveCall={async (txb) => {
+              await createBank(txb, STSUI_COIN.$typeName, ROOT);
+              await createVault(txb, STSUI_COIN.$typeName, ROOT);
+            }}
+          />
           <ViewObject objectId={BANK} display='View BANK for stSUI' />
           <ViewObject objectId={VAULT} display='View VAULT for stSUI' />
-          <Card title="Test toast" buttonText="fire" onClick={() => toast.success('Swap success!')} />
-          <StakeCard />
+          <MoveCallCard
+            title="Satke SUI"
+            buttonText="deposit"
+            moveCall={async (txb) => {
+              await sharbetMoveCall.stakeSuiToMintShasui(txb, {
+                address: wallet.address!,
+                amount: BigInt(100),
+              })
+            }}
+          />
         </div>
       </main>
     </div>
