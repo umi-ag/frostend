314bf662f66b8687f4d3c4fe19bbd16334c7b76b
fuyutarow <fuyutarow@gmail.com>
2023-09-21 18:40:25 +0900

    [ui] Refactor SwapTransactionButton

--- ui/src/components/SwapComponent/SwapTransactionButton.tsx
+++ ui/src/components/SwapComponent/SwapTransactionButton.tsx
@@ -9,10 +9,40 @@ import { isSHASUI } from 'src/moveCall/sharbet/shasui/structs';
 import { isSUI } from 'src/moveCall/sui/sui/structs';
 import { sharbetMoveCall } from 'src/sharbetLib';
 
-const whichCoinType = (coinType: string): "sui" | "shasui" | "sy" | "pt" | "yt" => {
-  if (isSUI(coinType)) { return 'sui' }
-  if (isSHASUI(coinType)) { return 'shasui' }
-  return whichCoinTypeIsSyPtYt(coinType)
+
+const whichCoinType = (coinType: string) => {
+  if (isSUI(coinType)) return 'sui';
+  if (isSHASUI(coinType)) return 'shasui';
+  return whichCoinTypeIsSyPtYt(coinType);
+}
+
+const getTransactionMessage = (sourceCoinType: string, targetCoinType: string) => {
+  return match([whichCoinType(sourceCoinType), whichCoinType(targetCoinType)])
+    .with(['sui', 'shasui'], () => "Stake SUI")
+    .with(['shasui', 'sui'], () => "Withdraw SUI")
+    .with(['sy', 'pt'], () => "Deposit stSUI")
+    .with(['pt', 'sy'], () => "Withdraw stSUI")
+    .with(['sy', 'yt'], () => "Deposit stSUI")
+    .with(['yt', 'sy'], () => "Withdraw stSUI")
+    .otherwise(() => { throw new Error('invalid coinType') });
+}
+
+const moveCallSwap = async (txb: TransactionBlock, args: {
+  sourceCoinType: string,
+  targetCoinType: string,
+  address: string,
+  // TODO: Uncomment this when bigint is supported
+  // amount: bigint,
+}) => {
+  const { address } = args
+  await match([whichCoinType(args.sourceCoinType), whichCoinType(args.targetCoinType)])
+    .with(['sui', 'shasui'], async () => { await sharbetMoveCall.stakeSuiToMintShasui(txb, { address, amount: BigInt(100) }) })
+    .with(['shasui', 'sui'], async () => { await sharbetMoveCall.burnShasuiToMintUnstsui(txb, { address, amount: BigInt(100) }) })
+    .with(['sy', 'pt'], async () => { await frostendMoveCall.swapSyToPt(txb, { address, amount: BigInt(10_000) }) })
+    .with(['pt', 'sy'], async () => { await frostendMoveCall.swapPtToSy(txb, { address, amount: BigInt(10_000) }) })
+    .with(['sy', 'yt'], async () => { await frostendMoveCall.swapSyToYt(txb, { address, amount: BigInt(10_000) }) })
+    .with(['yt', 'sy'], async () => { await frostendMoveCall.swapYtToSy(txb, { address, amount: BigInt(10_000) }) })
+    .otherwise(() => { throw new Error('invalid coinType') })
 }
 
 const SwapTransactionButton = () => {
@@ -24,33 +54,12 @@ const SwapTransactionButton = () => {
   const executeTransaction = async () => {
     if (!wallet.address) return;
     const txb = new TransactionBlock()
-
-    const { address } = wallet
-    await match([whichCoinType(sourceCoinType), whichCoinType(targetCoinType)])
-      .with(['sui', 'shasui'], async () => { await sharbetMoveCall.stakeSuiToMintShasui(txb, { address, amount: BigInt(100) }) })
-      .with(['shasui', 'sui'], async () => { await sharbetMoveCall.burnShasuiToMintUnstsui(txb, { address, amount: BigInt(100) }) })
-      .with(['sy', 'pt'], async () => { await frostendMoveCall.swapSyToPt(txb, { address, amount: BigInt(10_000) }) })
-      .with(['pt', 'sy'], async () => { await frostendMoveCall.swapPtToSy(txb, { address, amount: BigInt(10_000) }) })
-      .with(['sy', 'yt'], async () => { await frostendMoveCall.swapSyToYt(txb, { address, amount: BigInt(10_000) }) })
-      .with(['yt', 'sy'], async () => { await frostendMoveCall.swapYtToSy(txb, { address, amount: BigInt(10_000) }) })
-      .otherwise(() => { throw new Error('invalid coinType') })
-
+    moveCallSwap(txb, { sourceCoinType, targetCoinType, address: wallet.address })
     const r = await wallet.signAndExecuteTransactionBlock({
       // @ts-ignore
       transactionBlock: txb
     });
     noticeTxnResultMessage(r)
-  }
-
-  const displayMessage = () => {
-    return match([whichCoinType(sourceCoinType), whichCoinType(targetCoinType)])
-      .with(['sui', 'shasui'], async () => "Stake SUI")
-      .with(['shasui', 'sui'], async () => "Withdraw SUI")
-      .with(['sy', 'pt'], () => "Deposit stSUI")
-      .with(['pt', 'sy'], () => "Withdraw stSUI")
-      .with(['sy', 'yt'], () => "Deposit stSUI")
-      .with(['yt', 'sy'], () => "Withdraw stSUI")
-      .otherwise(() => { throw new Error('invalid coinType') })
   }
 
   const style = `text-lg bg-green-300 hover:bg-green-400 w-full p-4 rounded-full transition duration-200
@@ -63,7 +72,7 @@ const SwapTransactionButton = () => {
           await executeTransaction();
         }}
       >
-        {displayMessage()}
+        {getTransactionMessage(sourceCoinType, targetCoinType)}
       </button>
     ) : (
       <ConnectModal
