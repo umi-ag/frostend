04aafaca031c46256415a214098dad501cdb2000
imataka7 <t.imagawa@kakehashi.life>
2023-09-22 10:50:33 +0900

    Add stats card

--- ui/src/app/swap/page.tsx
+++ ui/src/app/swap/page.tsx
@@ -8,16 +8,16 @@ import { useTradeStore } from 'src/store/trade';
 const ToggleToken = () => {
   const { targetCoinType, setSwapPair } = useTradeStore()
 
-  const normal = 'w-full h-full rounded-xl bg-gray-50 text-gray-900';
+  const normal = 'w-full h-full rounded-xl bg-gray-50 text-gray-700';
   const ytClassName = targetCoinType === STSUI_YTCoinType
     ? 'w-full h-full rounded-xl border-2 border-blue-800 bg-blue-100 text-blue-900'
     : normal;
   const ptClassName = targetCoinType === STSUI_PTCoinType
-    ? 'w-full h-full rounded-xl border-2 border-green-800 bg-green-50 text-green-800'
+    ? 'w-full h-full rounded-xl border-2 border-green-800 bg-green-100 text-green-800'
     : normal;
 
   return (
-    <div className='grid grid-cols-2 min-h-[4em] rounded-xl text-black bg-gray-50 transition-all duration-200'>
+    <div className='grid grid-cols-2 min-h-[4em] rounded-xl text-black bg-gray-50 transition-all duration-200 shadow-xl'>
       <button className={ytClassName} onClick={() => setSwapPair(STSUI_SYCoinType, STSUI_YTCoinType)}>
         <p>YT: Long Yield APY</p>
         <p>50%</p>
@@ -30,19 +30,37 @@ const ToggleToken = () => {
   )
 }
 
+const StatsCard = () => {
+  const StatsRow = ({ title, value }: { title: string, value: string }) => (
+    <div className='text-center'>
+      <p className='text-gray-500 mb-2'>{title}</p>
+      <p className='font-semibold'>{value}</p>
+    </div>
+  );
+
+  return (
+    <div className='flex justify-around w-full rounded-xl bg-gray-50 text-gray-700 p-4 shadow-xl'>
+      <StatsRow title="Liquidity" value="$123,456,789" />
+      <StatsRow title="24h volume" value="$123,456,789" />
+      <StatsRow title="Underlying APY" value="10%" />
+      <StatsRow title="Fixed APY" value="10%" />
+    </div>
+  )
+}
 
 const Page = () => {
   return (
     <div className="h-screen bg-blue-500">
       <AppBar />
-      <main className="flex justify-center mt-[120px] flex-wrap">
+      <main className="flex justify-center mt-[120px] gap-8">
         <div>
-          <div className="text-white mb-16">
+          <div className="text-white mb-8">
             <ToggleToken />
           </div>
-          <div className=''>
+          <div className='mb-8'>
             <SwapComponent />
           </div>
+          <StatsCard />
         </div>
       </main>
     </div>
