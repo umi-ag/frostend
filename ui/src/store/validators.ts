ee9e982f8057327b8e39215c216367a9dc6bd5fc
imataka7 <t.imagawa@kakehashi.life>
2023-09-23 00:31:12 +0900

    Add swr

--- ui/src/store/validators.ts
+++ ui/src/store/validators.ts
@@ -1,14 +1,30 @@
 import { suiClient } from "src/config/sui";
-import { create } from "zustand";
-import { SuiSystemStateSummary } from "@mysten/sui.js/client";
+import useSWR, { preload } from 'swr';
 
-export const useSuiSystemState = create<{
-  suiSystemState: SuiSystemStateSummary | null;
-  fetch: () => void;
-}>((set) => ({
-  suiSystemState: null,
-  fetch: async () => {
-    const suiSystemState = await suiClient().getLatestSuiSystemState();
-    set({ suiSystemState });
+
+const fetchSuiSystemState = async () => {
+  const suiSystemState = await suiClient().getLatestSuiSystemState();
+  return suiSystemState;
+}
+
+preload('suiSystemState', fetchSuiSystemState);
+
+// export const useSuiSystemState = create<{
+//   suiSystemState: SuiSystemStateSummary | null;
+//   fetch: () => void;
+// }>((set) => ({
+//   suiSystemState: null,
+//   fetch: async () => {
+//     const suiSystemState = await suiClient().getLatestSuiSystemState();
+//     set({ suiSystemState });
+//   },
+// }));
+
+export const useSuiSystemState = () => useSWR(
+  'suiSystemState',
+  fetchSuiSystemState,
+  {
+    refreshInterval: 600_000, // 10 minutes
+    revalidateOnFocus: false,
   },
-}));
+);
