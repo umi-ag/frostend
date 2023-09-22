3673b4ce1b70fdc7fdd7d4ea5626e730ff90f351
imataka7 <t.imagawa@kakehashi.life>
2023-09-22 10:49:09 +0900

    Remove comments

--- ui/src/app/swap/page.tsx
+++ ui/src/app/swap/page.tsx
@@ -5,7 +5,7 @@ import { SwapComponent } from 'src/components/SwapComponent';
 import { STSUI_PTCoinType, STSUI_SYCoinType, STSUI_YTCoinType } from 'src/frostendLib';
 import { useTradeStore } from 'src/store/trade';
 
-const FaucetCard = () => {
+const ToggleToken = () => {
   const { targetCoinType, setSwapPair } = useTradeStore()
 
   const normal = 'w-full h-full rounded-xl bg-gray-50 text-gray-900';
@@ -18,36 +18,14 @@ const FaucetCard = () => {
 
   return (
     <div className='grid grid-cols-2 min-h-[4em] rounded-xl text-black bg-gray-50 transition-all duration-200'>
-      {/* <button className='w-full h-full rounded-xl border-2 border-blue-800 bg-blue-100 text-blue-900'> */}
       <button className={ytClassName} onClick={() => setSwapPair(STSUI_SYCoinType, STSUI_YTCoinType)}>
         <p>YT: Long Yield APY</p>
         <p>50%</p>
       </button>
-      {/* <button className='w-full h-full rounded-r-xl border border-green-800 bg-green-50 text-green-800'>PT: Fixed APY</button> */}
-      {/* <button className='w-full h-full bg-gray-50 text-gray-900'> */}
       <button className={ptClassName} onClick={async () => setSwapPair(STSUI_SYCoinType, STSUI_PTCoinType)}>
         <p>PT: Fixed APY</p>
         <p>50%</p>
       </button>
-      {/* <div className='flex flex-col items-center gap-3'>
-        <div className='text-black text-lg font-bold'>
-          stSUI 100
-        </div>
-        <div className='flex gap-3'>
-          <button
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg w-[200px]"
-            onClick={async () => setSwapPair(STSUI_SYCoinType, STSUI_YTCoinType)}
-          >
-            YT: Long Yield APY
-          </button>
-          <button
-            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg w-[200px]"
-            onClick={async () => setSwapPair(STSUI_SYCoinType, STSUI_PTCoinType)}
-          >
-            PT: Fixed APY
-          </button>
-        </div>
-      </div> */}
     </div>
   )
 }
@@ -60,7 +38,7 @@ const Page = () => {
       <main className="flex justify-center mt-[120px] flex-wrap">
         <div>
           <div className="text-white mb-16">
-            <FaucetCard />
+            <ToggleToken />
           </div>
           <div className=''>
             <SwapComponent />
